b9ea5aa062b67e282632b5cac51283bf7fcb6cf7
Javier Samper Arias <javier.samper.arias@nttdata.com>
2023-10-17 13:54:47 +0200

    comment i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,7 +1,7 @@
-import { getRequestConfig } from 'next-intl/server';
+/* import { getRequestConfig } from 'next-intl';
 export default getRequestConfig(async ({ locale }: any) => {
 
   return {
     messages: (await import(`./messages/${locale}.json`)).default
   }
-});
+}); */
