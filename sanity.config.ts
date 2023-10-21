51adb4ee505a2f52f8b096bb9fc7dc00bcd2246a
Javier Samper Arias <javier.samper.arias@nttdata.com>
2023-10-21 12:07:26 +0200

    change workflow version node

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,7 +8,7 @@ import { deskTool } from 'sanity/desk'
 import { documentInternationalization } from '@sanity/document-internationalization'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from './src/sanity/env'
+import { dataset, projectId } from './src/sanity/env'
 import { schema } from './src/sanity/schema'
 
 export default defineConfig({
@@ -21,7 +21,7 @@ export default defineConfig({
     deskTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion, defaultDataset: dataset }),
+    visionTool({ defaultDataset: dataset }),
     documentInternationalization({
       supportedLanguages: [
         { id: 'es', title: 'Español' },
--- src/sanity/lib/client/sanity.ts
+++ src/sanity/lib/client/sanity.ts
@@ -1,9 +1,8 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn, token } from '../../env';
+import { dataset, projectId, useCdn, token } from '../../env';
 
 export const client = createClient({
-  apiVersion,
   dataset,
   projectId,
   useCdn,
